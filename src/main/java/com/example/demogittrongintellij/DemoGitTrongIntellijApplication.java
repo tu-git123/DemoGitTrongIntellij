6cbe4924a98fbd99b320217d1da525b6fbd7c296
tu-git123 <huadinhtu2k@gmail.com>
2024-10-15 10:35:17 +0700

    Dự án demogit

---
+++ src/main/java/com/example/demogittrongintellij/DemoGitTrongIntellijApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogittrongintellij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitTrongIntellijApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGitTrongIntellijApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demogittrongintellij/DemoGitTrongIntellijApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demogittrongintellij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitTrongIntellijApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
